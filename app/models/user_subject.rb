00cd1c1bd5a011aacdeef5eb1f5f6ac8360f6614
hienbx94 <hienbx94@gmail.com>
2015-09-17 20:32:51 +0700

    user_update_done_task_v2

--- app/models/user_subject.rb
+++ app/models/user_subject.rb
@@ -4,4 +4,6 @@ class UserSubject < ActiveRecord::Base
   belongs_to :subject
   belongs_to :user_course
   has_many :user_tasks, dependent: :destroy
+
+  accepts_nested_attributes_for :user_tasks
 end
--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -2,4 +2,11 @@ class UserTask < ActiveRecord::Base
   belongs_to :task
   belongs_to :user_subject
   belongs_to :user
+
+  before_create :init_relation_user
+
+  private
+  def init_relation_user
+    self.user_id = self.user_subject.user_id
+  end
 end
