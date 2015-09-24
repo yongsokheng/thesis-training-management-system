c3703afd9788e5d16f178fd45e0617c2e4e52e3c
hienbx94 <hienbx94@gmail.com>
2015-09-24 08:18:06 +0700

    fix_ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   filter :locale
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
-  root "user_courses#index"
+  root "courses#show"
   devise_for :users, path: "auth", path_names: {sign_in: "login", sign_out: "logout"}
 
-  resources :user_courses, only: :index do
+  resources :courses, only: :show do
     resources :user_subjects, only: [:show, :update] do
       patch "/:status" => "subjects#update", as: :finish_subject
     end
   end
   resources :users, only: [:edit, :update, :show]
   resources :subjects, only: [:show]
-  resources :courses, only: [:show]
-  resources :tasks, onley: [:show]
+  resources :tasks, only: [:show]
 end
