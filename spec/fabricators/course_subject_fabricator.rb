b63316f8870ed211668dc51cf2032590b1ef45bf
Ngeng Chhengkim <chhengkimngeng@gmail.com>
2015-09-25 15:54:04 +0700

    Sua remake data

--- spec/fabricators/course_subject_fabricator.rb
+++
@@ -1,7 +0,0 @@
-Fabricator(:course_subject) do
-  CourseSubject.public_activity_off
-  status {%w(init progress finish).sample}
-
-  subject {Subject.all.sample}
-  course
-end
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:user) do
   name {sequence(:name) {|i| "User#{i}"}}
-  role {%w(admin supervisor trainee).sample}
+  role 2
   email {sequence(:email) {|i| "email#{i}@gmail.com"}}
   password "12345678"
   password_confirmation "12345678"
