aab37f9ffd749ecc2046bc22caa30880a5280d7e
lannp <nguyen.phuong.lan@framgia.com>
2015-10-01 14:40:54 +0700

    Edit layout index course and header admin

--- app/models/concerns/rails_admin_course.rb
+++ app/models/concerns/rails_admin_course.rb
@@ -13,8 +13,22 @@ module RailsAdminCourse
               href: "course/#{bindings[:object].id}/show_course")
           end
         end
-        field :description
-        field :status
+        field :start_date
+        field :end_date
+        field :status do
+          pretty_value do
+            status = bindings[:object]
+            class_label = if status.init?
+              "label label-success"
+            elsif status.progress?
+              "label label-info"
+            else status.finish?
+              "label label-default"
+            end
+            %{<div class="#{class_label}">#{status.status}</div>}.html_safe
+          end
+        end
+        field :leaders
       end
     end
   end
