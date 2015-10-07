7870d90942e784f3064e7861b980415a65fe0b84
lannp <nguyen.phuong.lan@framgia.com>
2015-10-07 13:13:48 +0700

    Fix-interface-supervisor

--- app/models/concerns/rails_admin_user.rb
+++ app/models/concerns/rails_admin_user.rb
@@ -5,12 +5,48 @@ module RailsAdminUser
       list do
         field :name do
           formatted_value do
-            bindings[:view].content_tag(:a, "#{bindings[:object].name}",
-              href: "user/#{bindings[:object].id}")
+            bindings[:view].content_tag :a, "#{bindings[:object].name}",
+              href: "user/#{bindings[:object].id}?locale=#{I18n.locale}"
           end
         end
         field :email
-        field :role
+        field :role do
+          pretty_value do
+            role = bindings[:object]
+            class_label = if role.trainee?
+              "label label-success"
+            elsif role.supervisor?
+              "label label-info"
+            else role.admin?
+              "label label-primary"
+            end
+            %{<div class="#{class_label}">#{role.role}</div>}.html_safe 
+          end
+        end
+        field :courses do
+          pretty_value do
+            value = ""
+            user = bindings[:object]
+            user.courses.each do |course|
+              if course.progress?
+                value = course.name
+              end
+            end
+            %{#{value}}.html_safe
+          end
+        end
+        field :course_leaders do
+          pretty_value do
+            value = ""
+            user = bindings[:object]
+            user.courses.each do |course|
+              if course.progress?           
+                value = course.leaders.first.name      
+              end
+            end
+             %{#{value}}.html_safe
+          end
+        end
       end
 
       edit do
