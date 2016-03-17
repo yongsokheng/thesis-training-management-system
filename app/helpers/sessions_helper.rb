946a023bc953c1efe0ad947d8c99b95596cf676c
NgocVu1994 <ngocvu.uet@gmail.com>
2016-03-17 16:24:37 +0700

    edit link11

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,12 @@ module SessionsHelper
   def current_user? user
     user == current_user
   end
+
+  def user_course_last_actived
+    current_user.courses.last
+  end
+
+  def permission_course_master
+    current_user.role.permissions.find_by model_class: "CourseMaster"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   filter :locale
-  root "courses#show"
+  root "static_pages#home"
+
   devise_for :users, path: "auth", path_names: {sign_in: "login", sign_out: "logout"}
 
   namespace :admin do
