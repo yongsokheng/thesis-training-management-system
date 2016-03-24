aa3e84cad630b0086c2506565d0f81216a4f924a
bapboy18 <duongnv.dhbk@gmail.com>
2016-03-24 16:24:35 +0700

    create evaluation

---
+++ app/assets/javascripts/admin/evaluation.js
@@ -0,0 +1,14 @@
+$(document).on("page:change", function() {
+  var total_point = 0;
+
+  $(".evaluation-detail-point").change(function() {
+    $(".evaluation-detail-point").each(function() {
+      if($(this).val() !== undefined) {
+        total_point += parseInt($(this).val());
+      }
+    });
+
+    document.getElementById("evaluation_total_point").value = total_point;
+    total_point = 0;
+  });
+});
---
+++ app/controllers/admin/evaluations_controller.rb
@@ -0,0 +1,47 @@
+class Admin::EvaluationsController < ApplicationController
+  load_and_authorize_resource
+  load_and_authorize_resource :user
+
+  before_action :load_template, only: [:new, :create, :edit, :update]
+  before_action :load_evaluation, only: [:edit, :update]
+
+  def new
+    @evaluation_templates.each do |template|
+      @evaluation.evaluation_details.build(name: template.name,
+        point: template.min_point, evaluation_template_id: template.id)
+    end
+  end
+
+  def create
+    if @evaluation.save
+      flash[:success] = flash_message "created"
+      redirect_to admin_users_path
+    else
+      flash[:success] = flash_message "not_created"
+      redirect_to :back
+    end
+  end
+
+  def update
+    if @evaluation.update_attributes evaluation_params
+      flash[:success] = flash_message "updated"
+      redirect_to admin_users_path
+    else
+      flash[:success] = flash_message "not_updated"
+      redirect_to :back
+    end
+  end
+
+  private
+  def evaluation_params
+    @evaluation_parms = params.require(:evaluation).permit Evaluation::ATTRIBUTES_PARAMS
+  end
+
+  def load_template
+    @evaluation_templates = EvaluationTemplate.all
+  end
+
+  def load_evaluation
+    @evaluation = @user.evaluation
+  end
+end
