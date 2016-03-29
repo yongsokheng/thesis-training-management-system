1571f756902b23bc834a25fe8b356e1d56a45775
VietMT <mai.tuan.viet@framgia.com>
2016-03-29 11:09:13 +0700

    Category University
    Fix error

---
+++ app/controllers/admin/universities_controller.rb
@@ -0,0 +1,47 @@
+class Admin::UniversitiesController < ApplicationController
+  load_and_authorize_resource, except: :show
+
+  def index
+    @university = University.new
+  end
+
+  def new
+  end
+
+  def create
+    if @university.save
+      flash[:success] = flash_message "created"
+      redirect_to admin_universities_path
+    else
+      flash[:failed] = flash_message "not_created"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @university.update_attributes university_params
+      flash[:success] = flash_message "updated"
+      redirect_to admin_universities_path
+    else
+      flash_message[:failed] = flash_message "not_updated"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @university.destroy
+      flash[:success] = flash_message "deleted"
+    else
+      flash[:failed] = flash_message "not_deleted"
+    end
+    redirect_to :back
+  end
+
+  private
+  def university_params
+    params.require(:university).permit University::ATTRIBUTES_PARAMS
+  end
+end
