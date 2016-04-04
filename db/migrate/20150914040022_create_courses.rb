3ff8ef58956e03fb2c90b7cb1415a5e334e47448
thiennv57 <thienbg93@gmail.com>
2016-04-04 09:57:55 +0700

    them truong cho course subject task

--- db/migrate/20150914040022_create_courses.rb
+++ db/migrate/20150914040022_create_courses.rb
@@ -2,7 +2,9 @@ class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
       t.string :name
+      t.string :avatar
       t.text :description
+      t.text :content
       t.integer :status, default: 0
       t.date :start_date
       t.date :end_date
--- db/migrate/20150914040024_create_subjects.rb
+++ db/migrate/20150914040024_create_subjects.rb
@@ -2,7 +2,9 @@ class CreateSubjects < ActiveRecord::Migration
   def change
     create_table :subjects do |t|
       t.string :name
+      t.string :avatar
       t.text :description
+      t.text :content
       t.integer :during_time
 
       t.timestamps null: false
