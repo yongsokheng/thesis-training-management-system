6b0085c3c5d1c6d97899715ce29f2a1593eb819a
thiennv57 <thienbg93@gmail.com>
2016-04-06 13:16:49 +0700

    fix bug Gem and rake task subject

--- spec/fabricators/subject_fabricator.rb
+++ spec/fabricators/subject_fabricator.rb
@@ -1,6 +1,7 @@
 Fabricator(:subject) do
-  name {sequence(:name) {|i| "subject#{i}"}}
+  name {sequence(:name) {|i| "Subject#{i}"}}
   description {Faker::Lorem.sentence}
+  content {Faker::Lorem.sentence}
   during_time {Faker::Number.between(1, 10)}
 
   after_create do |subject|
