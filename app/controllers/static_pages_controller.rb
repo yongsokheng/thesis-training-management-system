3d27fa14f2a39dac37d8a57b1d608d99894754f2
NgocVu1994 <ngocvu.uet@gmail.com>
2016-04-07 13:42:42 +0700

    design-root-admin

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    @count_course_active = Course.progress.count
+    @count_course = Course.count
+    @count_user_inprogress = UserCourse.course_progress.count
+    @count_user = User.count
   end
 
   def help
