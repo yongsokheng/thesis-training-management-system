7ccb1d0320d23431ffe201b9339a66c0f8195a95
yongsokheng <yongsokheng@ymail.com>
2016-04-18 10:54:52 +0700

    build user progress chart

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,10 @@ class StaticPagesController < ApplicationController
     @count_course = Course.count
     @count_user_inprogress = UserCourse.course_progress.count
     @count_user = User.count
+
+    @user_progress_name = UserProgress.pluck :name
+    @user_progress_total = UserProgress.includes(:profiles)
+      .map{|user_progress| user_progress.profiles.size}
   end
 
   def help
