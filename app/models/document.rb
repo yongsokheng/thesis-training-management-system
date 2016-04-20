657836a8e02e7444bb3f33416eb7dcc23235a7d8
LOCBINH <truonglocbinh93@gmail.com>
2016-04-20 14:25:06 +0700

    show document

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,12 @@ class Document < ActiveRecord::Base
   mount_uploader :content, DocumentUploader
 
   belongs_to :documentable, polymorphic: true
+
+  def name
+    if new_record?
+      super
+    else
+      self[:name] = self[:name].blank? ? content.file.filename : self[:name]
+    end
+  end
 end
